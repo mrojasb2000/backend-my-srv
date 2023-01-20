7627988cb87b96e7914d9b088f87e33fc25cea18
Mauricio Rojas <mrojasb2000@gmail.com>
2023-01-19 21:46:15 -0300

    feat: added foo path

--- main.py
+++ main.py
@@ -2,6 +2,12 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def root():
-    return {"message":"Hello World! from VIM"}
+    return {"message": "Hello World! from VIM"}
+
+
+@app.get("/foo")
+async def foo():
+    return {"message": "bar"}
